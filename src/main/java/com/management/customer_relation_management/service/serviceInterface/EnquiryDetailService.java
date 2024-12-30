19c7663a92261d82e4511e10fe1b892ecb9259ed
mayu-code <durbulemayu@gmail.com>
2024-12-30 13:42:20 +0530

    sync

--- src/main/java/com/management/customer_relation_management/service/serviceInterface/EnquiryDetailService.java
+++ src/main/java/com/management/customer_relation_management/service/serviceInterface/EnquiryDetailService.java
@@ -1,5 +1,7 @@
 package com.management.customer_relation_management.service.serviceInterface;
 
+import java.util.List;
+
 import com.management.customer_relation_management.entities.EnquiryDetail;
 import com.management.customer_relation_management.entities.EnquiryForm;
 
@@ -7,4 +9,6 @@ public interface EnquiryDetailService {
     
     EnquiryDetail addEnquiryDetail(EnquiryDetail detail,EnquiryForm enquiryForm);
     void deleteEnquiryDetail(long id);
+
+    List<EnquiryDetail> getAllEnquiryDetailByEnquiryForm(long id);
 }
