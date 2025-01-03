ddbfe59e86e14c3fcf115761162560a1c47f1ec4
mayu-code <durbulemayu@gmail.com>
2025-01-03 12:32:39 +0530

    sync

--- src/main/java/com/management/customer_relation_management/helper/DateTimeFormatter.java
+++ src/main/java/com/management/customer_relation_management/helper/DateTimeFormatter.java
@@ -10,8 +10,12 @@ public class DateTimeFormatter {
     public static String format(LocalDateTime dateTime) {
 
         java.time.format.DateTimeFormatter formatter = java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-
         return dateTime.format(formatter);
+    }
+
+    public static LocalDateTime deformat(String dateTimeString) {
+        java.time.format.DateTimeFormatter formatter = java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        return LocalDateTime.parse(dateTimeString, formatter);
     }
 
 }
